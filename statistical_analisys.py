7404958a9ec8bd8fe9c253ff73d102b39e996337
James <jamesmoo2090@yahoo.com>
2022-11-14 11:50:15 -0700

    Initial Commit - all dependencies installed

---
+++ statistical_analisys.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import xlrd as xlrd
+
+# read excel file https://www.geeksforgeeks.org/reading-excel-file-using-python/ - need to do this to load data before any analisys can happen
+
+
