dd22319aa192fe6072cdde43cb77ab5d397f2622
James <jamesmoo2090@yahoo.com>
2022-11-14 20:46:39 -0700

    read the excel file and select columns

--- statistical_analisys.py
+++ statistical_analisys.py
@@ -3,8 +3,20 @@ import numpy as np
 import pandas as pd
 import xlrd as xlrd
 
-# read excel file https://www.geeksforgeeks.org/reading-excel-file-using-python/ - need to do this to load data before any analisys can happen
+# read excel file links:
+# https://www.geeksforgeeks.org/reading-excel-file-using-python/
+# https://xlrd.readthedocs.io/en/latest/
+# https://www.digitalocean.com/community/tutorials/pandas-read_excel-reading-excel-file-in-python
 
-
+#global variables
+pathToFile = "C:\\my-sync\\MBA 6051\\Group Project - Student Grades\\Student Grades Data.xlsx"
+allColumns = ['gender', 'ethnicity', 'parental level of education', 'lunch',
+              'test preparation course', 'math score', 'reading score', 'writing score']
 def start():
-    print('starting analysis')
+    sheet_101_gender_male()
+
+def sheet_101_gender_male():
+    # header is: [excel row] - 1   => use Compsci where index starts at 0
+    excel = pd.read_excel(pathToFile, sheet_name="101 - Gender Male", header=13, usecols=allColumns)
+    print(excel)
+
